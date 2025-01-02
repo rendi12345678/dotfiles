60ff4719bf6d214f9ee1892e2fa9400b7850317d
rendi12345678 <hardleberg@gmail.com>
2025-01-02 10:59:19 +0700

    updates

--- .config/nvim/lua/rendi/plugins/coq.lua
+++ .config/nvim/lua/rendi/plugins/coq.lua
@@ -2,7 +2,7 @@ return {
   "neovim/nvim-lspconfig", -- REQUIRED: Native Neovim LSP integration
   lazy = false, -- REQUIRED: Start plugin at startup
   dependencies = {
-    -- COQ
+    -- COQ for autocomplete
     { "ms-jpq/coq_nvim", branch = "coq" },
     { "ms-jpq/coq.artifacts", branch = "artifacts" },
     { "ms-jpq/coq.thirdparty", branch = "3p" },
@@ -13,7 +13,6 @@ return {
       auto_start = true, -- Auto-start COQ
       keymap = {
         recommended = false,
-        bigger_preview = "",
         jump_to_mark = "<c-]>",
       },
       display = {
@@ -24,36 +23,99 @@ return {
     }
   end,
   config = function()
-    -- Keybindings
-    vim.api.nvim_set_keymap("i", "<Esc>", [[pumvisible() ? "\<C-e><Esc>" : "<Esc>"]], { expr = true, silent = true })
-    vim.api.nvim_set_keymap("i", "<C-c>", [[pumvisible() ? "\<C-e><C-c>" : "<C-c>"]], { expr = true, silent = true })
-    vim.api.nvim_set_keymap(
-      "i",
-      "<Tab>",
-      [[pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "<Tab>"]],
-      { expr = true, silent = true }
-    )
-    vim.api.nvim_set_keymap("i", "<C-j>", [[pumvisible() ? "\<C-n>" : "<Tab>"]], { expr = true, silent = true })
-    vim.api.nvim_set_keymap("i", "<C-k>", [[pumvisible() ? "\<C-p>" : "<BS>"]], { expr = true, silent = true })
-
-    -- LSP and COQ integration
     local lsp = require("lspconfig")
     local coq = require("coq")
 
-    local jdtls_config = {
-      cmd = { "java" },
-      root_dir = function(fname)
-        return require("lspconfig").util.root_pattern("pom.xml", "build.gradle", ".git")(fname) or vim.fn.getcwd()
-      end,
-    }
+    -- Utility function to simplify setup
+    local function setup_lsp(server, opts)
+      lsp[server].setup(coq.lsp_ensure_capabilities(opts or {}))
+    end
 
-    lsp.jdtls.setup(coq.lsp_ensure_capabilities(jdtls_config))
-    lsp.ts_ls.setup(coq.lsp_ensure_capabilities({}))
-    lsp.asm_lsp.setup(coq.lsp_ensure_capabilities({}))
-    lsp.tailwindcss.setup(coq.lsp_ensure_capabilities({}))
-    lsp.intelephense.setup(coq.lsp_ensure_capabilities({}))
-    lsp.html.setup(coq.lsp_ensure_capabilities({}))
-    lsp.jsonls.setup(coq.lsp_ensure_capabilities({}))
-    lsp.emmet_ls.setup(coq.lsp_ensure_capabilities({}))
+    -- Configure asm_lsp
+    setup_lsp("asm_lsp", {
+      cmd = { "asm-lsp" },
+      filetypes = { "asm", "s", "S" },
+      root_dir = function(fname)
+        return lsp.util.root_pattern(".git")(fname) or vim.fn.getcwd()
+      end,
+      settings = {
+        asm = {
+          enable = true,
+        },
+      },
+    })
+
+    -- Configure jdtls (Java)
+    local workspace_dir = vim.fn.expand("~/workspace-root/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"))
+    setup_lsp("jdtls", {
+      cmd = {
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xmx1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-jar",
+        vim.fn.expand("~/Downloads/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"),
+        "-configuration",
+        vim.fn.expand("~/.config/config_linux"),
+        "-data",
+        workspace_dir,
+      },
+      root_dir = function(fname)
+        return lsp.util.root_pattern(".git", "pom.xml", "build.gradle")(fname) or vim.fn.getcwd()
+      end,
+    })
+
+    -- Configure TypeScript LSP
+    setup_lsp("ts_ls", {
+      filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+      root_dir = lsp.util.root_pattern("package.json", "tsconfig.json", ".git") or vim.fn.getcwd(),
+    })
+
+    -- Configure Tailwind CSS
+    setup_lsp("tailwindcss", {
+      filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescriptreact" },
+      root_dir = function(fname)
+        return lsp.util.root_pattern("tailwind.config.js", ".git")(fname) or vim.fn.getcwd()
+      end,
+    })
+
+    -- Configure Intelephense (PHP)
+    setup_lsp("intelephense", {
+      root_dir = function(fname)
+        return lsp.util.root_pattern(".git", "composer.json")(fname) or vim.fn.getcwd()
+      end,
+    })
+
+    -- Configure HTML
+    setup_lsp("html", {
+      root_dir = lsp.util.root_pattern(".git") or vim.fn.getcwd(),
+    })
+
+    -- Configure JSON
+    setup_lsp("jsonls", {
+      settings = {
+        json = {
+          schemas = require("schemastore").json.schemas(),
+          validate = { enable = true },
+        },
+      },
+      root_dir = function(fname)
+        return lsp.util.root_pattern(".git", "package.json")(fname) or vim.fn.getcwd()
+      end,
+    })
+
+    -- Configure Emmet
+    setup_lsp("emmet_ls", {
+      filetypes = { "html", "css", "javascriptreact", "typescriptreact" },
+      root_dir = lsp.util.root_pattern(".git") or vim.fn.getcwd(),
+    })
   end,
 }
