47615bf212dc9eca4b802a008a2ba37e4000647b
rendi12345678 <hardleberg@gmail.com>
2025-02-27 13:19:47 +0700

    updates

--- .config/nvim/lua/rendi/plugins/rustaceannvim.lua
+++ .config/nvim/lua/rendi/plugins/rustaceannvim.lua
@@ -1,5 +1,26 @@
 return {
   "mrcjkb/rustaceanvim",
-  version = "^5", -- Recommended
-  lazy = false, -- This plugin is already lazy
+  version = "^5", -- Recommended version
+  lazy = false, -- Ensure it loads on startup
+  ft = { "rust" }, -- Load only for Rust files
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        autostart = true,
+        on_attach = function(client, bufnr)
+          local opts = { noremap = true, silent = true, buffer = bufnr }
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        end,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            checkOnSave = { command = "clippy" },
+            diagnostics = { enable = true },
+          },
+        },
+      },
+    }
+  end,
 }
